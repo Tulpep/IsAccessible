87380cfa36960b9307d2e0a1de3627234b1f2bb6
Ricardo Polo <ricardo.polo@tulpep.com>
2015-06-19 16:44:55 -0500

    Controller renamed

---
+++ Tulpep.IsAccessible/Tulpep.IsAccessible/Controllers/IsAccessibleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Tulpep.IsAccessible.Controllers
+{
+    public class IsAccessibleController : ApiController
+    {
+        // GET api/values
+        public string Get(string nombre, string sexo)
+        {
+            return "Hola " + nombre + ". Usted es: " + sexo;
+        }
+
+    }
+}
